5da2e7ebada457f4dbcb533e81ee907216dd7fd5
jackie <jackielin7789978@gmail.com>
2021-09-13 23:04:21 +0800

    move code from other folder & fix navbar issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
+import Sidebar from "../src/components/Sidebar";
+import { Home, Post, About, Login, NewPost } from "./pages";
+import { AuthContext } from "./context";
+import { getAuthToken } from "./utils";
+import { getMe } from "./WebApi";
 
-function App() {
+const PageWrapper = styled.div`
+  max-width: 100%;
+  font-family: "Roboto Mono", monospace;
+`;
+const MainArea = styled.div`
+  min-height: calc(100vh - 155px);
+  width: 50%;
+  margin-left: 25vw;
+  padding: 18px 36px;
+`;
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [isGettingUser, setIsGettingUser] = useState(false);
+  useEffect(() => {
+    const token = getAuthToken();
+    if (token !== "null") {
+      setIsGettingUser(true);
+      getMe().then((res) => {
+        setUser(res.data);
+        setIsGettingUser(false);
+      });
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ user, setUser, isGettingUser }}>
+      <Navbar />
+      <PageWrapper>
+        <Sidebar />
+        <MainArea>
+          <Switch>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/newpost">
+              <NewPost />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </MainArea>
+      </PageWrapper>
+      <Footer />
+    </AuthContext.Provider>
   );
 }
-
-export default App;
