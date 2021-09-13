a9f8bd81b3e88eb63a4d62c0b955bbfa1bdab27d
jackie <jackielin7789978@gmail.com>
2021-09-14 01:28:12 +0800

    add gh-pages

---
+++ src/components/App.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+import { Home, Post, About, Login, NewPost } from "../pages";
+import { AuthContext } from "../context";
+import { getAuthToken } from "../utils";
+import { getMe } from "../WebApi";
+
+const PageWrapper = styled.div`
+  max-width: 100%;
+  font-family: "Roboto Mono", monospace;
+`;
+const MainArea = styled.div`
+  min-height: calc(100vh - 155px);
+  width: 50%;
+  margin-left: 25vw;
+  padding: 18px 36px;
+`;
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [isGettingUser, setIsGettingUser] = useState(false);
+  useEffect(() => {
+    const token = getAuthToken();
+    if (token !== "null") {
+      setIsGettingUser(true);
+      getMe().then((res) => {
+        setUser(res.data);
+        setIsGettingUser(false);
+      });
+    }
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, setUser, isGettingUser }}>
+      <Navbar />
+      <PageWrapper>
+        <Sidebar />
+        <MainArea>
+          <Switch>
+            <Route path="/posts/:id">
+              <Post />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/newpost">
+              <NewPost />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </MainArea>
+      </PageWrapper>
+      <Footer />
+    </AuthContext.Provider>
+  );
+}
